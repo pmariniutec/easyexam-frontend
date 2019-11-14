e3fd1afb2ee3f06be1a09b6b70c97b54865588ab
María <mlovatonv@gmail.com>
2019-11-14 06:25:53 -0500

    search bar

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,12 @@
 			<router-view />
 		</v-content>
 
-    <SideBar v-if="!isAuthenticated" />
-
 	</v-app>
 </template>
 
 <script>
-import SideBar from '@/components/SideBar'
-
 export default {
 	name: 'App',
-  components: {
-    SideBar
-  },
 	data () { return {} },
 	created () {},
 	mounted () {}
