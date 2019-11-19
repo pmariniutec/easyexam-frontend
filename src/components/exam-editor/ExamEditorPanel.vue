6525b158fb28714f54cb514262e44435be20cc9c
María <mlovatonv@gmail.com>
2019-11-19 16:43:10 -0500

    New exam editor panel (#31)
    * lint stuff
    
    * remove huevadas
    
    * change create exam panel structure
    
    * no se que fue
    
    * add title edit
    
    * restructure exam editor

--- src/components/exam-editor/ExamEditorPanel.vue
+++ src/components/exam-editor/ExamEditorPanel.vue
@@ -1,86 +1,87 @@
 <template>
 	<v-card
-		class="create-exam-card"
+		class="panel-card"
 	>
 		<v-card-title
 			primary-title
 		>
-			<v-row>
-				<div class="title">
-					Create new exam
-				</div>
-			</v-row>
+			<v-container>
+				<v-row>
+					<v-col cols="6">
+            <v-text-field
+              v-model="exam.title"
+              label="Exam Title"
+              append-icon="mdi-pencil"
+              solo
+            /> 
+					</v-col>
+					<v-col
+						cols="6"
+						align="right"
+					>
+						<v-btn
+							color="secondary"
+							class="ma-2"
+						>
+							Preview
+						</v-btn>
+						<v-btn
+							color="primary"
+							class="ma-2"
+						>
+							Save
+						</v-btn>
+					</v-col>
+				</v-row>
+			</v-container>
 		</v-card-title>
 		<v-card-text>
-			<v-tabs
-				v-model="tab"
-			>
-				<v-tab
-					href="#tab-1"
-				>
-					Details
-				</v-tab>
-				<v-tab
-					href="#tab-2"
-				>
-					Questions
-				</v-tab>
-			</v-tabs>
+			<v-container>
+				<v-row>
+					<v-col>
+						<v-text-field
+							v-model="userName"
+							label="Teacher's name"
+							disabled
+						/>
+					</v-col>
+					<v-col>
+						<v-autocomplete
+							ref="course"
+							v-model="course"
+							:items="courses"
+							label="Course"
+							placeholder="Select..."
+						/>
+					</v-col>
+				</v-row>
+			</v-container>
 
-			<v-tabs-items
-				v-model="tab"
-			>
-				<v-tab-item
-					key="1"
-					value="tab-1"
-				>
-					<CreateExamPanelDetails />
-				</v-tab-item>
-				<v-tab-item
-					key="2"
-					value="tab-2"
-				>
-					<CreateExamPanelQuestions />
-				</v-tab-item>
-			</v-tabs-items>
+			<CreateExamPanelQuestions />
 		</v-card-text>
-		<v-col
-			cols="2"
-			align="right"
-			class="px-6"
-		>
-			<v-btn
-				color="primary"
-			>
-				Create
-			</v-btn>
-		</v-col>
-	</v-row>
 	</v-card>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 
-import CreateExamPanelDetails from '@/components/create_exams/CreateExamPanelDetails'
 import CreateExamPanelQuestions from '@/components/create_exams/CreateExamPanelQuestions'
-import CreateExamInput from '@/components/create_exams/CreateExamInput'
-import JQuery from 'jquery'
-let $ = JQuery
 
 export default {
-	name: 'CreateExamPanel',
+	name: 'ExamEditorPanel',
 	components: {
-		CreateExamPanelDetails,
 		CreateExamPanelQuestions
 	},
 	data: () => ({
+		course: 'CourseName - CourseCode',
+		courses: ['CourseName - CourseCode'],
+		userName: 'firstName lastName',
 		exam: {
-			  title: '',
+			  title: 'New Exam',
 			  questions: '',
 			  keywords: ''
 		},
-    error: '',
+		error: '',
 		tab: null,
 		texList: [{ mode: 'latex', tex: String.raw`
 				\documentclass{article}
@@ -137,137 +138,18 @@ export default {
 		...mapActions('exams', { createExamAction: 'createExam' }),
 
 		createExam: function () {
-      this.createExamAction(this.exam)
-        .then(() => ({}))
+			this.createExamAction(this.exam)
+				.then(() => ({}))
 			  .catch(() => {
 					this.error = 'Invalid Credentials'
-				})      
-    },
+				})
+		}
 	}
 }
-
-
-$(function(){
-
-  $('.tags input').on('focusout',function(){
-    var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,''); // allowed characters
-    if(txt) {
-      $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
-    }
-    this.value="";
-  }).on('keyup',function( e ){
-    // if: comma,enter (delimit more keyCodes with | pipe)
-    if(/(188|13|9)/.test(e.which)){ $(this).focusout();
-    }
-  });
-
-
-  $('.tags').on('click','.tag',function(){
-        if(confirm("Really delete this tag?")) $(this).remove();
-  });
-$('.tags').tagcloud({
-    tags: ['Quint', 'USS Indianapolis', 'Orca']
-});
-
-});
-
 </script>
 
 <style lang="scss" scoped>
-.create-exam-card {
-	background-color: #F3F3F6;
-	width: 80%;
-	left: 8%;
-	margin: 20px 0;
-	border-radius: 5% !important;
-	padding: 0 30px;
-  overflow-y: auto;
+.panel-card {
+  min-height: 96vh;
 }
-.title {
-	color: #23246E;
-  font-weight: 700 !important;
-  border-bottom-color: #23246E;
-  border-bottom-style: solid;
-	border-radius: 1px;
-  width: 100%;
-  margin: 20px 0px 10px 0px;
-  padding-bottom: 8px;
-}
-.input-card {
-	padding-top: 20px;
-}
-.row {
-	margin-bottom: 5px;
-}
-.row:after {
-	content: "";
-  display: table;
-  clear: both;
-}
-.column {
-  float: left;
-  width: 50%;
-	padding: 0px 20px;
-}
-
-	input.tags {
-	  border: none;
-	  box-shadow: none;
-	  width: auto;
-	  margin: 0;
-	  padding: 0;
-	  border-radius: 4px;
-	  color: #999;
-	  font-size: .9em;
-	  min-height:1.9em;
-	  display: inline-block;
-	  background: transparent;
-	}
-	input.tags:focus {
-	  outline:none;
-	  background: transparent;
-	}
-	.tags-bar{
-		-webkit-appearance: none;
-    width: 100%;
-    border: 1px solid #DBDBDB;
-		border-radius: 12px;
-    font-family: inherit;
-    padding: 7px;
-    height: 48px;
-    font-size: 16px;
-    font-weight: 500;
-    background-color: white;
-    color: #223254;
-  	transition: all .15s ease;
-	}
-
-	.tag {
-		position: relative;
-		display:inline-block;
-		border-radius: 4px;
-		background: red;
-		color: #fff;
-		margin: 0em 0.1em .1em .1em;
-		padding: .2em 1.3em .15em .45em;
-		transition: opacity 1s ease;
-	}
-
-					  .tag:after {
-					  position: absolute;
-					  display:block;
-					  top: .2em;
-					  right: 5px;
-					  content: "×";
-					  font-size: 1em;
-					  line-height: 1em;
-					  color: rgba(255,255,255,0.5);
-					  }
-
-					  .tag:hover{
-					   opacity: .8;
-					   cursor:pointer;
-					    transition: opacity 1s ease;
-					  }
-
 </style>
