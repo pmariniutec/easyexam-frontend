4ae4bdb45fae12b3d4112d01d116cbaf26915b38
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2019-11-20 00:55:58 -0500

    Removed unecessary classes in landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -58,10 +58,4 @@ export default {
 </script>
 
 <style lang="scss">
-.top-bar{
-box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-}
-.homepage-button{
-box-shadow: none;
-}
 </style>
