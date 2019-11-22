49ef6474c8dd05b104bf6d1718d988091200974f
María <mlovatonv@gmail.com>
2019-11-22 02:24:06 -0500

    jeje

--- src/services/session.js
+++ src/services/session.js
@@ -4,11 +4,10 @@ const CSRF_COOKIE_NAME = 'csrftoken'
 const CSRF_HEADER_NAME = 'X-CSRFToken'
 
 const session = axios.create({
-	baseURL: process.env.VUE_APP_API_URL || 'http://localhost:5000/api/',
+	baseURL: process.env.VUE_APP_API_URL || 'http://ec2-3-95-228-103.compute-1.amazonaws.com/api/',
 	headers: {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
-		'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYXJpYUBkdWNrLmNvbSIsImV4cCI6MTU3NDM3NDEwNSwiaWF0IjoxNTc0MzU2MTA1fQ.t-xJcwMVbqjZm9pkOB0zShSuxK93RD0rYz-TdHgJowyORhbEYx3aVvuzGuyveX9QuFfiUkQ8YPEaFjyaqt14MA'
 	},
 	xsrfCookieName: CSRF_COOKIE_NAME,
 	xsrfHeaderName: CSRF_HEADER_NAME,
