be28a0b854ece336739bc85c1d6d9fe316b53eae
Martin Carrasco <martin.carrasco@utec.edu.pe>
2019-11-21 23:01:19 -0500

    Changed style of stars rating

--- src/services/session.js
+++ src/services/session.js
@@ -4,7 +4,7 @@ const CSRF_COOKIE_NAME = 'csrftoken'
 const CSRF_HEADER_NAME = 'X-CSRFToken'
 
 const session = axios.create({
-	baseURL: process.env.VUE_APP_API_URL || 'http://localhost:5000/api/',
+	baseURL: process.env.VUE_APP_API_URL || 'http://ec2-3-95-228-103.compute-1.amazonaws.com/api/',
 	headers: {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
