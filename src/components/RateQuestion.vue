031c359a0870fac41109682e81b2565c07f9448e
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2019-11-22 09:32:48 -0500

    Added first version of user comments

--- src/components/RateQuestion.vue
+++ src/components/RateQuestion.vue
@@ -12,7 +12,19 @@
 						:text="question"
 					/>
 				</v-row>
-				<hr>
+				<b>
+					Comments
+				</b>
+				<v-divider :inset="inset"></v-divider>
+				<v-row>
+					<v-container>
+						<v-row v-for="item in comments">
+							<v-col cols="12">
+								{{item}}
+							</v-col>
+						</v-row>
+					</v-container>
+				</v-row>
 				<v-row class="mt-5">
 					<b>
 						Rating
@@ -96,6 +108,10 @@ export default {
 			25,50, 35,48, 40,45, 45,40, 48,35, 
 			50,25, 48,15, 45,10, 40,5, 35,2, 
 			25,0, 15,2, 10,5, 5,10, 2,15],
+		comments: ["Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam.", 
+			"Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam.", 
+			"Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam.", 
+			"Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam. Very good exam."],
 	}),
 	computed: {
 		getQuestion: function () {
@@ -112,128 +128,7 @@ export default {
 	}
 }
 
-
-$(function(){
-
-  $('.tags input').on('focusout',function(){
-    var txt= this.value.replace(/[^a-zA-Z0-9\+\-\.\#]/g,''); // allowed characters
-    if(txt) {
-      $(this).before('<span class="tag">'+ txt.toLowerCase() +'</span>');
-    }
-    this.value="";
-  }).on('keyup',function( e ){
-    // if: comma,enter (delimit more keyCodes with | pipe)
-    if(/(188|13|9)/.test(e.which)){ $(this).focusout();
-    }
-  });
-
-
-  $('.tags').on('click','.tag',function(){
-        if(confirm("Really delete this tag?")) $(this).remove();
-  });
-$('.tags').tagcloud({
-    tags: ['Quint', 'USS Indianapolis', 'Orca']
-});
-
-});
-
 </script>
 
 <style lang="scss" scoped>
-.create-exam-card {
-	background-color: #F3F3F6;
-	width: 80%;
-	left: 8%;
-	margin: 20px 0;
-	border-radius: 5% !important;
-	padding: 0 30px;
-  overflow-y: auto;
-}
-.title {
-	color: #23246E;
-  font-weight: 700 !important;
-  border-bottom-color: #23246E;
-  border-bottom-style: solid;
-	border-radius: 1px;
-  width: 100%;
-  margin: 20px 0px 10px 0px;
-  padding-bottom: 8px;
-}
-.input-card {
-	padding-top: 20px;
-}
-.row {
-	margin-bottom: 5px;
-}
-.row:after {
-	content: "";
-  display: table;
-  clear: both;
-}
-.column {
-  float: left;
-  width: 50%;
-	padding: 0px 20px;
-}
-
-	input.tags {
-	  border: none;
-	  box-shadow: none;
-	  width: auto;
-	  margin: 0;
-	  padding: 0;
-	  border-radius: 4px;
-	  color: #999;
-	  font-size: .9em;
-	  min-height:1.9em;
-	  display: inline-block;
-	  background: transparent;
-	}
-	input.tags:focus {
-	  outline:none;
-	  background: transparent;
-	}
-	.tags-bar{
-		-webkit-appearance: none;
-    width: 100%;
-    border: 1px solid #DBDBDB;
-		border-radius: 12px;
-    font-family: inherit;
-    padding: 7px;
-    height: 48px;
-    font-size: 16px;
-    font-weight: 500;
-    background-color: white;
-    color: #223254;
-  	transition: all .15s ease;
-	}
-
-	.tag {
-		position: relative;
-		display:inline-block;
-		border-radius: 4px;
-		background: red;
-		color: #fff;
-		margin: 0em 0.1em .1em .1em;
-		padding: .2em 1.3em .15em .45em;
-		transition: opacity 1s ease;
-	}
-
-					  .tag:after {
-					  position: absolute;
-					  display:block;
-					  top: .2em;
-					  right: 5px;
-					  content: "×";
-					  font-size: 1em;
-					  line-height: 1em;
-					  color: rgba(255,255,255,0.5);
-					  }
-
-					  .tag:hover{
-					   opacity: .8;
-					   cursor:pointer;
-					    transition: opacity 1s ease;
-					  }
-
 </style>
