b9350b021cbcabe408fb8bea451bd7c9e634664b
María <mlovatonv@gmail.com>
2019-11-21 23:52:15 -0500

    lint

--- src/components/LaTeXPreview.vue
+++ src/components/LaTeXPreview.vue
@@ -18,8 +18,8 @@ export default {
 	computed: {
 		generateLaTeX: function () {
 			let generator = new HtmlGenerator({ hyphenate: false })
-      let doc = parse(this.text, { generator: generator }).htmlDocument()
-      console.log(doc.documentElement.outerHTML)
+			let doc = parse(this.text, { generator: generator }).htmlDocument()
+			console.log(doc.documentElement.outerHTML)
 			return doc.documentElement.outerHTML
 		},
 		getText: function () {
