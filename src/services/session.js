f16f5539c287a54ae42e99427b35d77fe4bfa70b
dgcnz <canezdiego@gmail.com>
2019-11-26 23:57:50 -0500

    Handled expired token with axios interceptors

--- src/services/session.js
+++ src/services/session.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import store from '@/store/store'
+import router from '@/router'
 
 const CSRF_COOKIE_NAME = 'csrftoken'
 const CSRF_HEADER_NAME = 'X-CSRFToken'
@@ -11,6 +13,21 @@ const session = axios.create({
 	},
 	xsrfCookieName: CSRF_COOKIE_NAME,
 	xsrfHeaderName: CSRF_HEADER_NAME
+})
+
+session.interceptors.response.use((response) => {
+	// do something with the response data
+	console.log('Response was received')
+
+	return response
+}, (error) => {
+	if (error.response.status === 401) {
+		store.dispatch('auth/logout')
+		router.push('/login')
+		return null
+	}
+
+	return Promise.reject(error)
 })
 
 export default session
