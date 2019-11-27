bd7323ea9717ba6ce6e8ac2a02c0a8910fe4c868
María <mlovatonv@gmail.com>
2019-11-27 06:11:52 -0500

    change logout icon

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -74,7 +74,7 @@
 							@click="$emit('change-tab-event', 'logout')"
 						>
 							<v-list-item-icon style="margin:auto">
-								<v-icon size="30" color="red">
+								<v-icon size="30" color="red darken-3">
 									mdi-logout
 								</v-icon>
 							</v-list-item-icon>
